ec4ebc835333e6a605f304c6c555ad2807c8aa5f
Oleg Koretsky <oleg@radiokot.com.ua>
2023-11-21 18:11:52 +0200

    Add Svelte template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Cloudflare Tunnel client</p>
---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
+
+export default config;
